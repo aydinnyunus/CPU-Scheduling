8665af79301a53fe46d9306a142b6bb6d871f5b4
Yunus AYDIN <yunusaydin@strixeye.com>
2022-05-12 01:44:41 +0300

    Update files

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 
 const (
 	NUMBER_OF_PROCESS = 100
+	REALTIME = 0
+	HIGH = 1
+	NORMAL = 2
+	LOW = 3
 )
 
 type process struct {
 	actualBurstTime, estimatedBurstTime, arrivalTime float64
+	priority int64
 }
 
 func newProcess(arrivalTime float64) *process {
@@ -22,10 +27,12 @@ func newProcess(arrivalTime float64) *process {
 }
 
 var processList []*process
-var actualBurstTime []float64
+var _ []float64
 var estimatedBurstTime []float64
-var arrivalTime []int64
+var _ []int64
 var alpha =0.4
+var variance = float64(0)
+
 
 func calculateBurst(i int64) float64{
 	process := alpha * processList[i-1].actualBurstTime + (1-alpha) * processList[i-1].estimatedBurstTime
@@ -38,6 +45,33 @@ func main() {
 	Burst time(nth process) = alpha * actual burst time (n-1)th process + (1-alpha) * estimated burst time(n-1)th process
 
 	Where alpha is a constant ; 0 <= alpha =< 1
+
+	3 priority queue
+	Average Queue Length
+	Average Waiting Time
+	Average Arrival Time
+	Average Turnaround Time
+	FIFO Round Robin
+	Poisson enter
+	Exponential leave
+	 */
+	now := time.Now()
+	sec := float64(now.Unix()) // number of seconds since January 1, 1970 UTC
+	s1 := rand.NewSource(time.Now().UnixNano())
+
+	r1 := rand.New(s1)
+	last := float64(sec) + r1.Float64() * float64(1000)
+	diff := last - float64(sec)
+
+	/*
+	//Or however many you might need + buffer.
+	c := make(chan int, 300)
+
+	//Push
+	c <- value
+
+	//Pop
+	x <- c
 	 */
 	for i := 1 ; i <= NUMBER_OF_PROCESS; i++ {
 		s1 := rand.NewSource(time.Now().UnixNano())
@@ -46,19 +80,34 @@ func main() {
 		burst := r1.Float64()*100
 		fmt.Printf("\nActual Burst Time : %f\n", burst)
 
-		now := time.Now()
-		sec := now.Unix()      // number of seconds since January 1, 1970 UTC
-		fmt.Printf("Arrival Time : %d\n", sec)
-		process_1 := newProcess(float64(sec))
-		if len(processList) > 1 {
-			process_1.estimatedBurstTime = calculateBurst(int64(len(processList) - 1))
+
+		if len(processList) > 1{
+			mean := NUMBER_OF_PROCESS/diff
+			fmt.Println(mean)
+			poisson := Poisson{Lambda: float64(mean)}
+			variance = poisson.Variance()
+			fmt.Printf("%f\n", poisson.StdDev())
+			fmt.Printf("%f\n", variance)
 		}
-		process_1.actualBurstTime = burst
-		processList = append(processList, process_1)
-		fmt.Printf("Process %d Estimated Burst Time : %f\n", i,process_1.estimatedBurstTime)
+
+		fmt.Printf("Arrival Time : %d\n", float64(sec) + variance)
+
+		sec = float64(sec) + variance
+		process1 := newProcess(float64(sec) + variance)
+		if i == 1{
+			process1.arrivalTime = float64(sec)
+		}
+		if len(processList) > 1 {
+			process1.estimatedBurstTime = calculateBurst(int64(len(processList) - 1))
+		}
+		r1 = rand.New(s1)
+		priority := r1.Int63n(3)
+		process1.priority = priority
+		process1.actualBurstTime = burst
+		processList = append(processList, process1)
+
+		fmt.Printf("Process %d Estimated Burst Time : %f\n", i, process1.estimatedBurstTime)
 	}
 	fmt.Println(processList)
-
-
 
 }
