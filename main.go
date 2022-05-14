b598943d244da056bd878214ff0992fc271cd407
Yunus AYDIN <yunusaydin@strixeye.com>
2022-05-14 13:17:10 +0300

    Update files

--- main.go
+++ main.go
@@ -4,164 +4,137 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/agoussia/godes"
 )
 
+//Input Parameters
 const (
-	NUMBER_OF_PROCESS = 100
-	REALTIME = 0
-	HIGH = 1
-	NORMAL = 2
-	LOW = 3
+	SHUTDOWN_TIME    = 8 * 60.
+	ALPHA 			 = float64(0.4)
+
 )
 
-var processList []*process
-var _ []float64
-var estimatedBurstTime []float64
-var _ []int64
-var alpha =0.4
-var variance = float64(0)
-var total_time_counted = float64(0)
-var wait_time = float64(0)
-var turnaround_time = float64(0)
-var totalTime = float64(0)
+// the arrival and service are two random number generators for the exponential  distribution
+var arrival = godes.NewExpDistr(true)
+var service = godes.NewExpDistr(true)
+
+// true when any counter is available
+var counterSwt = godes.NewBooleanControl()
+
+// FIFO Queue for the arrived customers
+var processArrivalQueue = godes.NewFIFOQueue("0")
+var allProcessQueue = godes.NewFIFOQueue("0")
 
 
+var tellers *Queues
+var measures [][]float64
+var titles = []string{
+	"Elapsed Time",
+	"Queue Length",
+	"Queueing Time",
+	"Service Time",
+}
 
-type process struct {
-	actualBurstTime, estimatedBurstTime, arrivalTime, remainingTime float64
+var availableQueues int = 0
+
+// the Queues is a Passive Object represebting resource
+type Queues struct {
+	max int
+}
+
+func (queues *Queues) Catch(customer *Process) {
+	for {
+		counterSwt.Wait(true)
+		if processArrivalQueue.GetHead().(*Process).id == customer.id {
+			break
+		} else {
+			godes.Yield()
+		}
+	}
+	availableQueues++
+	if availableQueues == queues.max {
+		counterSwt.Set(false)
+	}
+}
+
+func (queues *Queues) Release() {
+	availableQueues--
+	counterSwt.Set(true)
+}
+
+// Process the Process is a Runner
+type Process struct {
+	*godes.Runner
+	id int
+	actualBurstTime, estimatedBurstTime, arrivalTime, remainingTime, serviceTime, waitTime, turnAroundTime float64
 	priority int64
 }
 
-func newProcess(arrivalTime float64) *process {
-
-	p := process{arrivalTime: arrivalTime}
-	return &p
-}
-
-
-func calculateBurst(i int64) float64{
-	process := alpha * processList[i-1].actualBurstTime + (1-alpha) * processList[i-1].estimatedBurstTime
-	estimatedBurstTime = append(estimatedBurstTime, process)
-	return process
-}
-
-func roundRobin(totalTime float64){
-	timeQuantum := float64(5)
-	isCalculated := false
-	for totalTime != 0{
-		for i, _ := range processList {
-			if processList[i].remainingTime <= timeQuantum && processList[i].remainingTime >= 0{
-				total_time_counted += processList[i].remainingTime
-				totalTime -= processList[i].remainingTime
-
-				processList[i].remainingTime = 0
-			} else if processList[i].remainingTime > 0 {
-
-				processList[i].remainingTime -= timeQuantum
-				totalTime -= timeQuantum
-				total_time_counted += timeQuantum
-			}
-			if processList[i].remainingTime == 0 && isCalculated {
-
-				wait_time += total_time_counted - processList[i].arrivalTime - processList[i].actualBurstTime
-				turnaround_time += total_time_counted - processList[i].arrivalTime
-				isCalculated = true
-			}
-		}
-	}
-}
-func priorityQueue(){
+func (process *Process) Run() {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	no := service.Get(1. / r1.Float64())
+	process.serviceTime = float64(time.Now().Unix()) + no
+	a0 := godes.GetSystemTime()
+	tellers.Catch(process)
+	a1 := godes.GetSystemTime()
+	processArrivalQueue.Get()
+	qlength := float64(processArrivalQueue.Len())
+	godes.Advance(no)
+	a2 := godes.GetSystemTime()
+	tellers.Release()
+	collectionArray := []float64{a2 - a0, qlength, a1 - a0, a2 - a1}
+	measures = append(measures, collectionArray)
+	fmt.Printf("Estimated Burst Time %f", process.estimatedBurstTime)
+	fmt.Println()
+	fmt.Printf("Arrival Time %f", process.arrivalTime)
+	fmt.Println()
 
 }
 
-func calculateAvgArr(){
+func getProcessByID(){
 
 }
 
-func calculateAvgWait(){
-
-}
-
-func calculateAvgTurn(){
-
-}
-
-func calculateAvgQueue(){
-	
+func calculateBurst(process *Process) float64{
+	calculate := ALPHA * process.actualBurstTime + (1-ALPHA) * process.estimatedBurstTime
+	return calculate
 }
 
 func main() {
-	/*
-	Burst time(nth process) = alpha * actual burst time (n-1)th process + (1-alpha) * estimated burst time(n-1)th process
-
-	Where alpha is a constant ; 0 <= alpha =< 1
-
-	3 priority queue
-	Average Queue Length
-	Average Waiting Time
-	Average Arrival Time
-	Average Turnaround Time
-	FIFO Round Robin
-	Poisson enter
-	Exponential leave
-	 */
-	now := time.Now()
-	sec := float64(now.Unix()) // number of seconds since January 1, 1970 UTC
-	s1 := rand.NewSource(time.Now().UnixNano())
-
-	r1 := rand.New(s1)
-	last := float64(sec) + r1.Float64() * float64(1000)
-	diff := last - float64(sec)
-
-	/*
-	//Or however many you might need + buffer.
-	c := make(chan int, 300)
-
-	//Push
-	c <- value
-
-	//Pop
-	x <- c
-	 */
-
-	/* INITIALIZATION */
-	for i := 1 ; i <= NUMBER_OF_PROCESS; i++ {
+	measures = [][]float64{}
+	tellers = &Queues{3}
+	godes.Run()
+	counterSwt.Set(true)
+	count := 0
+	for {
 		s1 := rand.NewSource(time.Now().UnixNano())
-
 		r1 := rand.New(s1)
-		burst := r1.Float64()*100
-		fmt.Printf("\nActual Burst Time : %f\n", burst)
-
-
-		if len(processList) > 1{
-			mean := NUMBER_OF_PROCESS/diff
-			fmt.Println(mean)
-			poisson := Poisson{Lambda: float64(mean)}
-			variance = poisson.Variance()
-			fmt.Printf("%f\n", poisson.StdDev())
-			fmt.Printf("%f\n", variance)
+		no := arrival.Get(1. / r1.Float64())
+		customer := &Process{&godes.Runner{}, count,0,0,float64(time.Now().Unix())+no,0,0,0,0,0}
+		processArrivalQueue.Place(customer)
+		allProcessQueue.Place(customer)
+		if count > 1{
+			customer.estimatedBurstTime= calculateBurst(customer)
 		}
-
-		fmt.Printf("Arrival Time : %d\n", float64(sec) + variance)
-
-		sec = float64(sec) + variance
-		process1 := newProcess(float64(sec) + variance)
-		if i == 1{
-			process1.arrivalTime = float64(sec)
+		godes.AddRunner(customer)
+		godes.Advance(no)
+		if godes.GetSystemTime() > SHUTDOWN_TIME {
+			break
 		}
-		if len(processList) > 1 {
-			process1.estimatedBurstTime = calculateBurst(int64(len(processList) - 1))
-		}
-		r1 = rand.New(s1)
-		priority := r1.Int63n(3)
-		process1.priority = priority
-		process1.actualBurstTime = burst
-		processList = append(processList, process1)
-		totalTime += processList[i].actualBurstTime
-		fmt.Printf("Process %d Estimated Burst Time : %f\n", i, process1.estimatedBurstTime)
+		count++
 	}
-
-	roundRobin(totalTime)
-	fmt.Println(processList)
-
+	godes.WaitUntilDone() // waits for all the runners to finish the Run()
+	collector := godes.NewStatCollector(titles, measures)
+	collector.PrintStat()
+	fmt.Printf("Finished \n")
 }
+
+/* OUTPUT
+Variable		#	Average	Std Dev	L-Bound	U-Bound	Minimum	Maximum
+Elapsed Time	944	 2.591	 1.959	 2.466	 2.716	 0.005	11.189
+Queue Length	944	 2.411	 3.069	 2.215	 2.607	 0.000	13.000
+Queueing Time	944	 1.293	 1.533	 1.195	 1.391	 0.000	 6.994
+Service Time	944	 1.298	 1.247	 1.219	 1.378	 0.003	 7.824
+*/
